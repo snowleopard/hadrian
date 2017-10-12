beea14313da974a1b81e540c74655bfceee132e2
Ben Gamari <ben@smart-cactus.org>
2017-10-12 10:30:31 -0400

    gmp: Pass include directories to Haskell compiler
    Previously the include directories weren't being included in the arguments
    used to invoke GHC when building the in-tree GMP. This caused the build to fail
    with missing include files.
    
    I considered for a moment using Cabal's `--extra-include-dirs` flag to pass
    these directories. However, it's important that we
    Instead of duplicating the include directory logic for the Haskell compiler,
    I just use Cabal's --extra-include-dirs flag, ensuring that

--- src/Settings/Packages/IntegerGmp.hs
+++ src/Settings/Packages/IntegerGmp.hs
@@ -12,13 +12,17 @@ import Rules.Gmp
 integerGmpPackageArgs :: Args
 integerGmpPackageArgs = package integerGmp ? do
     path <- expr gmpBuildPath
+    topDir <- expr topDirectory
     let includeGmp = "-I" ++ path -/- "include"
     gmpIncludeDir <- getSetting GmpIncludeDir
     gmpLibDir     <- getSetting GmpLibDir
+    let -- are we building an in-tree GMP?
+        inTreeGmp = null gmpIncludeDir && null gmpLibDir
     mconcat [ builder Cc ? arg includeGmp
 
             , builder GhcCabal ? mconcat
-              [ (null gmpIncludeDir && null gmpLibDir) ?
+              [ inTreeGmp ?
                 arg "--configure-option=--with-intree-gmp"
               , arg ("--configure-option=CFLAGS=" ++ includeGmp)
-              , arg ("--gcc-options="             ++ includeGmp) ] ]
+              , arg ("--extra-include-dirs="++ topDir -/- path -/- "include")
+              ] ]
