c26cba3939baee788b6d1ad308a38c2d8545c77f
Moritz Angermann <moritz.angermann@gmail.com>
2017-11-24 12:07:49 +0800

    More leaf nodes.

--- src/Hadrian/Builder/Tar.hs
+++ src/Hadrian/Builder/Tar.hs
@@ -11,16 +11,9 @@
 module Hadrian.Builder.Tar (TarMode (..), args) where
 
 import Development.Shake
-import Development.Shake.Classes
-import GHC.Generics
 import Hadrian.Expression
 
--- | Tar can be used to 'Create' an archive or 'Extract' from it.
-data TarMode = Create | Extract deriving (Eq, Generic, Show)
-
-instance Binary   TarMode
-instance Hashable TarMode
-instance NFData   TarMode
+import Hadrian.Builder.Types (TarMode (..))
 
 -- | Default command line arguments for invoking the archiving utility @tar@.
 args :: (ShakeValue c, ShakeValue b) => TarMode -> Args c b
