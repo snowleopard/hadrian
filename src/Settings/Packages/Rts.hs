12397019f9dbc3b503158bf41f42b960f0a29e00
Moritz Angermann <moritz.angermann@gmail.com>
2018-01-09 09:20:24 +0800

    Adds `USE_LIBFFI_FOR_ADJUSTORS` flag to the RTS package.

--- src/Settings/Packages/Rts.hs
+++ src/Settings/Packages/Rts.hs
@@ -124,6 +124,7 @@ rtsPackageArgs = package rts ? do
           , Profiling `wayUnit` way          ? arg "-DPROFILING"
           , Threaded  `wayUnit` way          ? arg "-DTHREADED_RTS"
           , notM ghcWithSMP ? arg "-DNOSMP"
+          , useLibFFIForAdjustors ? arg "-DUSE_LIBFFI_FOR_ADJUSTORS"
 
           , inputs ["//RtsMessages.c", "//Trace.c"] ?
             arg ("-DProjectVersion=" ++ show projectVersion)
