b83c38d40148ff446e71a33314a55767147f3db3
Alp Mestanogullari <alp@well-typed.com>
2018-06-15 11:20:42 +0200

    attempt at setting some more test arguments correctly

--- src/Rules/Test.hs
+++ src/Rules/Test.hs
@@ -71,7 +71,12 @@ needTestsuiteBuilders = do
     need targets
   where
     needfile :: Stage -> Package -> Action FilePath
-    needfile stage pkg = programPath =<< programContext stage pkg
+    needfile stage pkg
+      -- TODO (Alp): we might sometimes need more than vanilla!
+      -- This should therefore depend on what test ways
+      -- we are going to use, I suppose?
+      | isLibrary pkg = pkgConfFile (Context stage pkg (read "v"))
+      | otherwise = programPath =<< programContext stage pkg
 
 needTestBuilders :: Action ()
 needTestBuilders = do
